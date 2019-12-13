9cdeae76b2fb93ae1b5dc9750e542d783dee75d2
lukas2005 <lmarianski@protonmail.com>
2019-12-13 11:26:00 +0100

    st

---
+++ types/node-7z.d.ts
@@ -0,0 +1,9 @@
+declare module "node-7z" {
+    // export function add(name: string): any;
+    // export function delete(): any;
+    // export function extract(): any;
+    // export function extractFull(): any;
+    // export function rename(): any;
+    // export function hash(): any;
+    // export function list(): any;
+}
